e3fb85ab9f235b507531bc760432dd1ecc68a015
Travis Umbel <Umbelth94@gmail.com>
2024-01-26 15:30:59 -0600

    license icons and links now work

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,10 +1,32 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  switch (license) {
+    case 'MIT':
+      return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+    case 'Apache 2.0':
+      return '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+    case 'GPL 3.0':
+      return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
+    case 'BSD 3':
+      return `[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)`;
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  switch (license) {
+    case 'MIT':
+      return '[MIT](https://opensource.org/licenses/MIT)';
+    case 'Apache 2.0':
+      return '[Apache 2.0](https://opensource.org/licenses/Apache-2.0)';
+    case 'GPL 3.0':
+      return '[GPL 3.0](https://www.gnu.org/licenses/gpl-3.0)';
+    case 'BSD 3':
+      return '[BSD 3](https://opensource.org/licenses/BSD-3-Clause)';
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
@@ -13,8 +35,15 @@ function renderLicenseSection(license) {
   if (license === 'None'){
     return '';
   } else {
+    const licenseBadge = renderLicenseBadge(license);
+    const licenseLink = renderLicenseLink(license);
+    console.log(licenseBadge, licenseLink);
     return `## License
-    This project is licensed under the ${license} license.`
+${licenseBadge}
+
+${licenseLink}
+
+This project is licensed under the ${license} license.`
   }
 }
 
@@ -47,7 +76,7 @@ ${contribution}
 ${tests}
 
 ## Questions
-If you have any questions about this application, please contact me at ${email} or visit my Github profile at https://github.com/${github}`
+If you have any questions about this application, please contact me at <${email}> or visit my Github profile at https://github.com/${github}`
 }
 
 function test(){
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 const markdown = require('./generateMarkdown.js');
 // TODO: Create an array of questions for user input
-const questions = [];
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
@@ -11,116 +11,64 @@ function writeToFile(fileName, data) {
     err? console.log(err) : console.log('Success!'))
 }
 
-inquirer
-.prompt([
-   {
-      type: 'input',
-      name: 'title',
-      message: 'What is the title of your project?'
-   },
-   {
-      type: 'input',
-      name: 'description',
-      message: 'Please provide a description of your project.'
-   },
-   {
-      type: 'input',
-      name: 'installation',
-      message: 'Please provide installation instructions.'
-   },
-   {
-      type: 'input',
-      name: 'usage',
-      message: 'Please provide usage instructions.'
-   },
-   {
-      type: 'input',
-      name: 'contribution',
-      message: 'Please provide contribution guidelines.'
-   },
-   {
-      type: 'input',
-      name: 'test',
-      message: 'Please provide test instructions.'
-   },
-   {
-      type:'input',
-      name:'email',
-      message:'Please provide your email address.'
-   },
-   {
-      type:'input',
-      name:'github',
-      message:'Please provide your GitHub username.'
-   },
-   { type:'list',
-name:'license',
-choices:['MIT','Apache 2.0','GPL 3.0','BSD 3','None']}
-])
-.then((data) => {
-   console.log(data.license)
-    const markdownContent = markdown.generateMarkdown(data);
-    console.log(markdownContent);
-      console.log(data);
-      writeToFile('README.md', markdownContent);
-  })
-.catch((err) => console.log(err));
-
-
-
 // TODO: Create a function to initialize app
 function init() {
-//     markdown.test();
-//     inquirer
-//       .prompt([
-//          {
-//             type: 'input',
-//             name: 'title',
-//             message: 'What is the title of your project?'
-//          },
-//          {
-//             type: 'input',
-//             name: 'description',
-//             message: 'Please provide a description of your project.'
-//          },
-//          {
-//             type: 'input',
-//             name: 'installation',
-//             message: 'Please provide installation instructions.'
-//          },
-//          {
-//             type: 'input',
-//             name: 'usage',
-//             message: 'Please provide usage instructions.'
-//          },
-//          {
-//             type: 'input',
-//             name: 'contribution',
-//             message: 'Please provide contribution guidelines.'
-//          },
-//          {
-//             type: 'input',
-//             name: 'test',
-//             message: 'Please provide test instructions.'
-//          },
-//          {
-//             type:'input',
-//             name:'email',
-//             message:'Please provide your email address.'
-//          },
-//          {
-//             type:'input',
-//             name:'github',
-//             message:'Please provide your GitHub username.'
-//          }
-//       ])
-//       .then((data) => {
-//             console.log(data);
-//             console.log(markdown.generateMarkdown(data));
-//             // console.log(markdownContent);
-//             writeToFile('README.md', markdownContent);
-//         })
-//       .catch((err) => console.log(err));
+   const questions = [
+      {
+         type: 'input',
+         name: 'title',
+         message: 'What is the title of your project?'
+      },
+      {
+         type: 'input',
+         name: 'description',
+         message: 'Please provide a description of your project.'
+      },
+      {
+         type: 'input',
+         name: 'installation',
+         message: 'Please provide installation instructions.'
+      },
+      {
+         type: 'input',
+         name: 'usage',
+         message: 'Please provide usage instructions.'
+      },
+      {
+         type: 'input',
+         name: 'contribution',
+         message: 'Please provide contribution guidelines.'
+      },
+      {
+         type: 'input',
+         name: 'tests',
+         message: 'Please provide test instructions.'
+      },
+      {
+         type:'input',
+         name:'email',
+         message:'Please provide your email address.'
+      },
+      {
+         type:'input',
+         name:'github',
+         message:'Please provide your GitHub username.'
+      },
+      { type:'list',
+   name:'license',
+   choices:['MIT','Apache 2.0','GPL 3.0','BSD 3','None']}
+   ];
+
+inquirer
+   .prompt(questions)
+   .then((data) => {
+      console.log(data.license)
+      const markdownContent = markdown.generateMarkdown(data);
+      console.log(markdownContent);
+         console.log(data);
+         writeToFile('README-Template.md', markdownContent);
+   })
+   .catch((err) => console.log(err));
 }
 
 // Function call to initialize app
